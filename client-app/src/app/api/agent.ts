300945ff766e4201dc1058ac0cf4364703af6a76
Szaszik-Viktor <szaszik.viktor@gmail.com>
2023-03-26 00:14:03 +0100

    end of section 13

--- client-app/src/app/api/agent.ts
+++ client-app/src/app/api/agent.ts
@@ -13,6 +13,14 @@ const sleep = (delay: number) => {
 
 axios.defaults.baseURL = "http://localhost:5000/api"
 
+const responseBody = <T>(response: AxiosResponse<T>) => response.data;
+
+axios.interceptors.request.use(config => {
+    const token = store.commonStore.token;
+    if (token && config.headers) config.headers.Authorization = `Bearer ${token}`;
+    return config;
+})
+
 axios.interceptors.response.use(async response => {
     await sleep(1000);
     return response;
@@ -53,20 +61,18 @@ axios.interceptors.response.use(async response => {
 
 })
 
-const responseBody = <T>(response: AxiosResponse<T>) => response.data;
-
 const requests = {
     get: <T>(url: string) => axios.get<T>(url).then(responseBody),
-    post: <T>(url: string, body: {}) => axios.post<T>(url).then(responseBody),
-    put: <T>(url: string, body: {}) => axios.put<T>(url).then(responseBody),
+    post: <T>(url: string, body: {}) => axios.post<T>(url, body).then(responseBody),
+    put: <T>(url: string, body: {}) => axios.put<T>(url, body).then(responseBody),
     del: <T>(url: string) => axios.delete<T>(url).then(responseBody)
 }
 
 const Activities = {
     list: () => requests.get<Activity[]>('/activities'),
     details: (id: string) => requests.get<Activity>(`/activities/${id}`),
-    create: (activity: Activity) => axios.post<void>('/activities', activity),
-    update: (activity: Activity) => axios.put<void>(`/activities/${activity.id}/`, activity),
+    create: (activity: Activity) => requests.post<void>('/activities', activity),
+    update: (activity: Activity) => requests.put<void>(`/activities/${activity.id}/`, activity),
     delete: (id: string) => axios.delete<void>(`/activities/${id}`)
 }
 
